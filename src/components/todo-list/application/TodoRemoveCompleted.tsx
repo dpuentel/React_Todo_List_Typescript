d99b894f4804dc275fb57a3fd71d281d147d33cf
David Puente <david.puente@sdweb.es>
2021-05-24 20:24:04 +0200

    Convert TodoRemoveCompleted to class

--- src/components/todo-list/application/TodoRemoveCompleted.tsx
+++ src/components/todo-list/application/TodoRemoveCompleted.tsx
@@ -1,11 +1,27 @@
+import React from 'react';
+
 interface Props {
   deleteCompleted: () => Promise<void>;
 }
 
-export function TodoRemoveCompleted({ deleteCompleted }: Props) {
+/* export function TodoRemoveCompleted({ deleteCompleted }: Props) {
   const handleClearCompleted = async () => {
     await deleteCompleted();
   };
 
   return <button onClick={handleClearCompleted}>➖ Clear completed tasks</button>;
+} */
+
+export class TodoRemoveCompleted extends React.Component<Props> {
+  /* constructor(props: Props) {
+    super(props);
+  } */
+
+  private handleClearCompleted: () => Promise<void> = async () => {
+    await this.props.deleteCompleted();
+  };
+
+  render() {
+    return <button onClick={this.handleClearCompleted}>➖ Clear completed tasks</button>;
+  }
 }
